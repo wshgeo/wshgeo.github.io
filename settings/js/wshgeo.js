6d7fddddb222e82346634807931a7e5997c66c78
wshgeo <37795488+wshgeo@users.noreply.github.com>
2019-12-01 19:48:03 +0800

    Update wshgeo.js

--- settings/js/wshgeo.js
+++ settings/js/wshgeo.js
@@ -1,7 +1,7 @@
 (function(){
     $(document).ready(function(){
         var tabs = config.tab;
-//         console.log($("html").css("height"));
+        // console.log($("html").css("height"));
         for(var i=0; i<tabs.length; i++){
             var tabParamter = {
                 className : config.tabStyleName[tabs[i].style],
@@ -12,7 +12,8 @@
                 tabWidth : "50%"
             };
             tabParamter.tabWidth = (tabs[i].style == "small") ? ((100/tabs.length) + '%') : "50%";
-            var tabHtmlStr = "<div class='"+ tabParamter.className +"' style='width:"+ tabParamter.tabWidth +";height:100%;'>";
+            tabParamter.maxHeight = ($("html").css("width") < "500px") ? "350px" : "9999px";
+            var tabHtmlStr = "<div class='"+ tabParamter.className +"' style='width:"+ tabParamter.tabWidth +";height:100%;min-width:400px;max-height:"+ tabParamter.maxHeight +"'>";
             tabHtmlStr += "<div class='tab-normal-title'><table align='center'><tr><td>"+ tabParamter.title +"</td></tr><tr><td><img src='"+ tabParamter.titleImgUrl +"'></img></td></tr></table></div>"
             tabHtmlStr += "<a class='tab-normal-body' href='"+ tabParamter.bodyHref +"'><table align='center'><tr><td><img src='"+ tabParamter.bodyImgUrl +"'></img></td></tr></table></a>"
             tabHtmlStr += "</div>"
